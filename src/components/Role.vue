0afe62f745798f6e303a6dc201f5dc57a8bce14d
NishantTheProgrammer <knishant113@gmail.com>
2022-11-13 22:37:44 +0530

    added role component

---
+++ src/components/Role.vue
@@ -0,0 +1,47 @@
+<script lang="ts">
+
+export default {
+    data() {
+        return {
+            roles: [
+                "Programmer",
+                "React Dev",
+                "Django Dev",
+                "Freelancer",
+            ],
+            role: 'Programmer',
+            write: false,
+            selected: 0
+        }
+    },
+    mounted() {
+        setInterval(() => {
+            if (!this.write) {
+                if (this.role.length !== 0) {
+                    this.role = this.role.slice(0, this.role.length - 1);
+                }
+                else {
+                    
+                    this.selected =  (this.selected + 1) % this.roles.length;
+                    this.write = true;
+                }
+            }
+            else {
+                if (this.role !== this.roles[this.selected]) {
+                    this.role = this.roles[this.selected].slice(0, this.role.length + 1);
+                }
+                else {
+                    this.write = false;
+                }
+            }
+        }, 100);
+    }
+}
+
+
+</script>
+
+
+<template>
+    <span class="text-black py-1 px-3 rounded-md bg-green-400">{{ role }}</span>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import NavBar from '@/components/NavBar.vue';
-
+import Role from '@/components/Role.vue';
 </script>
 
 <template>
@@ -8,18 +8,18 @@ import NavBar from '@/components/NavBar.vue';
   <main>
     <section class=" h-[88vh] flex items-end justify-between px-20">
       <div class="w-3/5 h-full p-40">
-        <span class="text-black py-1 px-3 rounded-md bg-green-400">Front end developer</span>
+        <Role></Role>
         <h1 class="text-4xl mt-5">Mobile Application developer <br><span class="text-xl">at Max vision solutions PVT. LTD.</span></h1>
-        <p class="text-sm text-gray-400 mt-20">I'm one of the best problem solvers. (I have HackerRank’s 5th Star gold badge In problem solving)</p>
+        <p class="text-sm text-gray-400 mt-20 font-mono">A curious tech guy with awesome technical skills and great thinking ability I like to build logic behind the seens rather than using any ready made tool.</p>
         <a href="mailto:knishant113@gmail.com" class="block mt-20 text-green-400 underline font-bold">LET'S TALK!</a>
         <div class="grid grid-cols-2 mt-20">
-          <div class="flex gap-3">
+          <div class="flex gap-3 items-center">
             <h3 class="text-6xl">2</h3>
-            <p class="text-lg text-gray-400">Years <br>Experience</p>
+            <p class="text-lg text-gray-400 font-mono">Years <br>Experience</p>
           </div>
-          <div class="flex gap-3">
+          <div class="flex gap-3 items-center">
             <h3 class="text-6xl">18</h3>
-            <p class="text-lg text-gray-400">Projects  <br>completed</p>
+            <p class="text-lg text-gray-400 font-mono">Projects  <br>completed</p>
           </div>
         </div>
       </div>
