f59f1a47259bf242c11918dfee2deca9e0b904e4
NishantTheProgrammer <knishant113@gmail.com>
2022-11-13 16:46:21 +0530

    added fontawesome

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,17 +1,17 @@
 <template>
-    <nav class="flex justify-between py-10 px-20">
+    <nav class="flex justify-between items-center py-10 px-20">
         <h1 class="text-2xl"><span class="font-extrabold">Nishant</span>The<span class="font-extrabold">Programmer</span></h1>
-        <div class="flex gap-3">
-            <RouterLink to="/">Home</RouterLink>
-            <RouterLink to="/about">About</RouterLink>
-            <RouterLink to="/project">Project</RouterLink>
-            <RouterLink to="/query">Query</RouterLink>
+        <div class="flex gap-5 text-sm">
+            <RouterLink to="/"><font-awesome-icon icon="home" /> Home</RouterLink>
+            <RouterLink to="/about"><font-awesome-icon icon="address-card" /> About</RouterLink>
+            <RouterLink to="/project"><font-awesome-icon icon="list-check" /> Project</RouterLink>
+            <RouterLink to="/query"><font-awesome-icon icon="clipboard-question" /> Query</RouterLink>
         </div>
-        <div class="flex gap-3">
-            <a target="_blank" href="https://github.com/NishantTheProgrammer">Github</a>
-            <a target="_blank" href="https://www.linkedin.com/in/nishanttheprogrammer">Linkedin</a>
-            <a target="_blank" href="https://www.instagram.com/nishanttheprogrammer">Instagram</a>
-            <a target="_blank" href="https://wa.me/917838716089?text=Hi%20Nishant%2C%20I%20found%20your%20contact%20at%20nishanttheprogrammer.com%20I%20have%20some%20queries">WhatsApp</a>
+        <div class="flex gap-5 text-sm">
+            <a target="_blank" href="https://github.com/NishantTheProgrammer"><font-awesome-icon :icon="['fab', 'github']" /> Github</a>
+            <a target="_blank" href="https://www.linkedin.com/in/nishanttheprogrammer"><font-awesome-icon :icon="['fab', 'linkedin']" /> Linkedin</a>
+            <a target="_blank" href="https://www.instagram.com/nishanttheprogrammer"><font-awesome-icon :icon="['fab', 'instagram']" /> Instagram</a>
+            <a target="_blank" href="https://wa.me/917838716089?text=Hi%20Nishant%2C%20I%20found%20your%20contact%20at%20nishanttheprogrammer.com%20I%20have%20some%20queries"><font-awesome-icon :icon="['fab', 'whatsapp']" /> WhatsApp</a>
         </div>
     </nav>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faAddressCard, faClipboardQuestion, faHome, faListCheck, faPhone } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faInstagram, faLinkedin, faWhatsapp } from "@fortawesome/free-brands-svg-icons";
 
 import App from "./App.vue";
 import router from "./router";
 
 import "./assets/main.css";
 
+library.add(faWhatsapp, faLinkedin, faInstagram, faGithub, faAddressCard, faHome, faClipboardQuestion, faListCheck);
+
 const app = createApp(App);
+
+app.component("font-awesome-icon", FontAwesomeIcon);
 
 app.use(createPinia());
 app.use(router);
